4854c31aa8bef57738a8ee7a504359f44a73e9ec
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-10-04 21:28:58 +0000

    amended styles for github data containers and content

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,9 @@ function repoInformationHTML(repos) {
 
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+    
     let username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h5>Please enter a GitHub username in the input field above</h5>`);
